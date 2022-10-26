c82a2171f1679ef00d28122864c76836dd1704ae
Diogo Marques <djcm.marques.95@gmail.com>
2022-10-26 12:57:12 +0100

    pai para filho, componentes LWC

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,8 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
 
+  
     counter = 0;
+    //@api counter =0;
+    _currentCount = 0;
+    priorCount = 0;
+    @api
+    get counter() {
+      return this._currentCount;
+    }
+    set counter(value) {
+      this.priorCount = this._currentCount;
+      this._currentCount = value;
+    }
+
     handleIncrement() {
       this.counter++;
     }
@@ -13,4 +26,9 @@ export default class Numerator extends LightningElement {
       const factor = event.detail;
       this.counter *= factor;
     }
+    @api
+    maximizeCounter(){
+      this.counter +=10000;
+    }
+
 }
